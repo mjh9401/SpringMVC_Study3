4fa71b300a93c0daf367abc23a87fcf747518768
mjh9401 <mjh9401@naver.com>
2023-06-02 16:47:40 +0900

    Bean Validation 적용

--- src/main/java/hello/itemService/domain/item/Item.java
+++ src/main/java/hello/itemService/domain/item/Item.java
@@ -1,16 +1,27 @@
 package hello.itemService.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 
 @Data
+//@ScriptAssert(lang = "javascript", script="_this.price * _this.quantity >=10000",message = "총합이 10000원 넘게 입력해주세요")
 public class Item {
 
     private Long id;
+    @NotBlank
     private String itemName;
+    @NotNull
+    @Range(min=1000,max=1000000)
     private Integer price;
+    @Max(9999)
+    @NotNull
     private Integer quantity;
 
     private Boolean open;// 판매여부
