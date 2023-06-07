4560fc79ab8f740ba39126f937075b60cd5e00a2
mjh9401 <mjh9401@naver.com>
2023-06-07 11:01:24 +0900

    로그인 인증 서블릿 필터 -> 인터셉터로 변경

--- src/main/java/hello/itemService/WebConfiguration.java
+++ src/main/java/hello/itemService/WebConfiguration.java
@@ -2,16 +2,31 @@ package hello.itemService;
 
 import hello.itemService.web.filter.LogFilter;
 import hello.itemService.web.filter.LoginCheckFilter;
+import hello.itemService.web.interceptor.LogInterceptor;
+import hello.itemService.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(new LogInterceptor())
+               .order(1).addPathPatterns("/**")
+               .excludePathPatterns("/css/**","/*.ico","/error");
+       registry.addInterceptor(new LoginCheckInterceptor())
+               .order(2)
+               .addPathPatterns("/**")
+               .excludePathPatterns("/","/members/add","/login","logout","/css/**","/*.ico","/error");
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -21,8 +36,8 @@ public class WebConfiguration {
         return filterFilterRegistrationBean;
     }
 
-    @Bean
-    public FilterRegistrationBean loginChekcFilter(){
+//    @Bean
+    public FilterRegistrationBean loginCheckFilter(){
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
         filterFilterRegistrationBean.setOrder(2);
